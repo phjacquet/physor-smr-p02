6bd9015e85d4e9f17116c44024df7c8a7e865e6c
pjacquet.physor@gmail.com <pjacquet.physor@gmail.com@28ece7da-dfb0-8059-279c-eb3c89b8111b>
2014-06-02 13:29:48 +0000

    Last results

--- problem1_TcycleOptim/DragonJddGen/CoreParameters.d
+++ problem1_TcycleOptim/DragonJddGen/CoreParameters.d
@@ -1,11 +1,11 @@
 *** COMBUSTIBLE
-Alpha	1.096493	(E1/E2)
+Alpha	1.22784	(E1/E2)
 E1	VAR.Comb.E1
 E2	VAR.Comb.E2
 VecteurPu Magnox.3000
 
 *** COEUR GEOMETRIE
-HauteurCoeur		86.8211500
-PNucleaire	386.552750
+HauteurCoeur		224.657000
+PNucleaire	309.166580
 RayonCoeur		100.
 	
