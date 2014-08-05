56db9afbd3d8544801cdb0f2c01fc6762329d535
pjacquet.physor@gmail.com <pjacquet.physor@gmail.com@28ece7da-dfb0-8059-279c-eb3c89b8111b>
2014-08-05 22:13:22 +0000

    Major bug correction in reflector model 2 set of results added

--- trunk/problem1_TcycleOptim/DragonJddGen/CoreParameters.d
+++ trunk/problem1_TcycleOptim/DragonJddGen/CoreParameters.d
@@ -1,11 +1,11 @@
 *** COMBUSTIBLE
-Alpha	1.12145	(E1/E2)
+Alpha	.027029	(E1/E2)
 E1	VAR.Comb.E1
 E2	VAR.Comb.E2
-VecteurPu PWR.43000
+VecteurPu Magnox.3000
 
 *** COEUR GEOMETRIE
-HauteurCoeur		88.969730
-PNucleaire	101.892970
+HauteurCoeur		115.03980
+PNucleaire	125.758630
 RayonCoeur		100.
 	
